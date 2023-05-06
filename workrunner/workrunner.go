6fff2f97d67b6eacd2e05aab643c7099a0689495
yousuf <yusuffaseel@gmail.com>
2023-05-06 21:43:14 +0530

    remove worker from work runner after completion

--- workrunner/workrunner.go
+++ workrunner/workrunner.go
@@ -34,6 +34,19 @@ func (wr *WorkRunner[K, V]) Observe(key K, fn func() V) <-chan V {
 	}
 	log.Printf("[DEBUG] <WID: %s> Observing worker\n", wkr.Id().String())
 	wkr.Subscribe(func(v V) {
+		// Though workers are reusable, it's better to keep workers map light as possible.
+		wr.mu.RLock()
+		_, ok := wr.m[key]
+		wr.mu.RUnlock()
+		if ok {
+			wr.mu.Lock()
+			_, ok = wr.m[key] // Re-check after placing a full lock.
+			if ok {
+				delete(wr.m, key)
+			}
+			wr.mu.Unlock()
+		}
+
 		c <- v
 	})
 	return c
